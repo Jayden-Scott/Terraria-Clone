c2e1153df86ea538345c1b24b83d1882315c1634
Dirk Schut <dirk@mrdls.com>
2014-08-26 01:34:15 +0200

    Added init method to button which sets the lablestyle

--- TerrariaClone/src/com/mrdls/tc/screen/gui/Button.java
+++ TerrariaClone/src/com/mrdls/tc/screen/gui/Button.java
@@ -5,16 +5,23 @@ import java.awt.Point;
 public class Button extends Label{
 
 	private static final long serialVersionUID = -304233725144918747L;
+	
+	public void init(){
+		this.setStyle(LabelStyles.button);
+	}
 
 	public Button(String text, Point location) {
 		super(text, location);
+		init();
 	}
 	
 	public Button(String text){
 		super(text);
+		init();
 	}
 
 	public Button(String text, int x, int y){
 		super(text, x, y);
+		init();
 	}
 }
