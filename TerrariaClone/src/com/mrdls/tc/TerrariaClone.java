13d874a8bf29daf93b8193b04ebd0783b3037ad1
Dirk Schut <dirk@mrdls.com>
2014-08-25 18:10:47 +0200

    Added a basic Hello, World! applet

---
+++ TerrariaClone/src/com/mrdls/tc/TerrariaClone.java
@@ -0,0 +1,14 @@
+package com.mrdls.tc;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class TerrariaClone extends Applet{
+
+	private static final long serialVersionUID = 5145659858916661907L;
+
+	public void paint(Graphics g){
+		g.drawString("Hello, World!", 10, 20);
+	}
+
+}
