7de68f9871d76405a8605c4889b22ac8276a17e6
Dirk Schut <dirk@mrdls.com>
2014-08-26 01:45:17 +0200

    Changed the border width in the button lablestyle

--- TerrariaClone/src/com/mrdls/tc/screen/gui/LabelStyles.java
+++ TerrariaClone/src/com/mrdls/tc/screen/gui/LabelStyles.java
@@ -7,5 +7,5 @@ public class LabelStyles {
 	public static LabelStyle label = new LabelStyle().setColor(Color.black).setHasBackground(false).setHasBorder(false);
 	public static LabelStyle success = new LabelStyle().setColor(new Color(30,143,33)).setBgcolor(new Color(185,253,187));
 	
-	public static LabelStyle button = new LabelStyle().setColor(Color.black).setBgcolor(Color.lightGray).setBorderWidth(5).setBorderColor(Color.darkGray);
+	public static LabelStyle button = new LabelStyle().setColor(Color.black).setBgcolor(Color.lightGray).setBorderWidth(2).setBorderColor(Color.darkGray);
 }
