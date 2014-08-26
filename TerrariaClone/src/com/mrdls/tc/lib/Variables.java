276c34f6d6c0a270268fb9b6ab65b3f42093e275
Dirk Schut <dirk@mrdls.com>
2014-08-26 23:12:15 +0200

    turned on debug mode

--- TerrariaClone/src/com/mrdls/tc/lib/Variables.java
+++ TerrariaClone/src/com/mrdls/tc/lib/Variables.java
@@ -14,7 +14,7 @@ import com.mrdls.tc.screen.Screen;
 
 public class Variables {
 
-	public static boolean debug = false;
+	public static boolean debug = true;
 	
 	public static JFrame frame;
 	public static Screen screen;
