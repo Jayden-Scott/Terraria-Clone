2820a5b0a6126a5482f51a5746cbb1c129e2f1a2
Dirk Schut <dirk@mrdls.com>
2014-08-26 22:42:33 +0200

    Removed test content from main menu screen state

--- TerrariaClone/src/com/mrdls/tc/screen/states/common/StateMainMenu.java
+++ TerrariaClone/src/com/mrdls/tc/screen/states/common/StateMainMenu.java
@@ -1,10 +1,7 @@
 package com.mrdls.tc.screen.states.common;
 
-import com.mrdls.tc.screen.gui.Button;
 import com.mrdls.tc.screen.gui.Label;
-import com.mrdls.tc.screen.gui.ElementStyles;
 import com.mrdls.tc.screen.states.ScreenState;
-import com.mrdls.tc.screen.states.StateManager;
 
 public class StateMainMenu extends ScreenState{
 
@@ -13,16 +10,11 @@ public class StateMainMenu extends ScreenState{
 	public StateMainMenu() {
 		super("Main Menu");
 		
-		addElement("label", new Label("Label", 10, 20));
-		addElement("success", new Label("Success", 10, 40).setStyle(ElementStyles.success));
-		addElement("button", new Button("Button", 10, 60));
+		
 	}
 	
 	public void tick(){
 		super.tick();
 		
-		if(getElement("button").isClicked()){
-			StateManager.setState("Credits");
-		}
 	}
 }
