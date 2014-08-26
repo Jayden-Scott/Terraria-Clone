5885d93ef3f82521f57dcec4fed98f214185f9c5
Dirk Schut <dirk@mrdls.com>
2014-08-26 16:30:09 +0200

    Added a label to the credit state

--- TerrariaClone/src/com/mrdls/tc/screen/states/common/StateCredits.java
+++ TerrariaClone/src/com/mrdls/tc/screen/states/common/StateCredits.java
@@ -1,11 +1,14 @@
 package com.mrdls.tc.screen.states.common;
 
+import com.mrdls.tc.screen.gui.Label;
 import com.mrdls.tc.screen.states.ScreenState;
 
 public class StateCredits extends ScreenState{
-
+	
 	public StateCredits() {
 		super("Credits");
+		
+		addElement("label", new Label("You have reached the credits section. 1 internet cookie for you.", 10, 20));
 	}
 
 }
