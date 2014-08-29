1ec15fca07c0f617cea6d6d82a1f47a4f45a32b3
Dirk Schut <dirk@mrdls.com>
2014-08-29 21:45:16 +0200

    Added a button to the credits game state which returns the user to the main menu

--- TerrariaClone/src/com/mrdls/tc/screen/states/common/StateCredits.java
+++ TerrariaClone/src/com/mrdls/tc/screen/states/common/StateCredits.java
@@ -1,14 +1,24 @@
 package com.mrdls.tc.screen.states.common;
 
+import com.mrdls.tc.screen.gui.Button;
 import com.mrdls.tc.screen.gui.Label;
 import com.mrdls.tc.screen.states.ScreenState;
+import com.mrdls.tc.screen.states.StateManager;
 
 public class StateCredits extends ScreenState{
 	
 	public StateCredits() {
 		super("Credits");
 		
-		addElement("label", new Label("You have reached the credits section. 1 internet cookie for you.", 10, 20));
+		addElement("back", new Button("Back to Main Menu", 10, 20));
+		addElement("label", new Label("You have reached the credits section. 1 internet cookie for you.", 10, 50));
 	}
 
+	public void tick(){
+		super.tick();
+		
+		if(getElement("back").isClicked()){
+			StateManager.setState("Main Menu");
+		}
+	}
 }
