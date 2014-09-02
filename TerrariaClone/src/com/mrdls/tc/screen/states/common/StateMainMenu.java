a74dc06789d19a1ea5d4c3c8aaa41bce8d131d1f
Dirk Schut <dirk@mrdls.com>
2014-09-02 09:24:18 +0200

    Added a version label to the main menu screen state

--- TerrariaClone/src/com/mrdls/tc/screen/states/common/StateMainMenu.java
+++ TerrariaClone/src/com/mrdls/tc/screen/states/common/StateMainMenu.java
@@ -1,7 +1,9 @@
 package com.mrdls.tc.screen.states.common;
 
+import com.mrdls.tc.lib.Finals;
 import com.mrdls.tc.lib.Variables;
 import com.mrdls.tc.screen.gui.ButtonList;
+import com.mrdls.tc.screen.gui.Label;
 import com.mrdls.tc.screen.states.ScreenState;
 import com.mrdls.tc.screen.states.StateManager;
 
@@ -11,6 +13,7 @@ public class StateMainMenu extends ScreenState{
 		super("Main Menu");
 		
 		addElement("Menu", new ButtonList().addButton("New Game").addButton("Load Game").addButton("Settings").addButton("Credits").addButton("Exit"));
+		addElement("Version", new Label("Version: " + Finals.PROJECT_VERSION));
 		
 		sizeChanged();
 	}
@@ -32,5 +35,8 @@ public class StateMainMenu extends ScreenState{
 	public void sizeChanged(){
 		getElement("Menu").x = Variables.screenSize.width / 2 - getElement("Menu").width /2;
 		getElement("Menu").y = Variables.screenSize.height / 2 - getElement("Menu").height /2;
+		
+		getElement("Version").x = Variables.screenSize.width - getElement("Version").width - 5;
+		getElement("Version").y = Variables.screenSize.height - getElement("Version").height - 5;
 	}
 }
